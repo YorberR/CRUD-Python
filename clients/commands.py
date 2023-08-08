5f1c6ff24a3005735d6ff79cb837bc2a80a3ea89
YorberR <yorberrojas15@gmail.com>
2023-08-08 17:47:20 -0400

    Update y delete en la version 2

--- clients/commands.py
+++ clients/commands.py
@@ -11,21 +11,21 @@ def clients():
 
 @clients.command()
 @click.option('-n', '--name', 
-              type=str,
-              prompt=True,
-              help='The client name')
+            type=str,
+            prompt=True,
+            help='The client name')
 @click.option('-c', '--company', 
-              type=str,
-              prompt=True,
-              help='The client company')
+            type=str,
+            prompt=True,
+            help='The client company')
 @click.option('-e', '--email', 
-              type=str,
-              prompt=True,
-              help='The client email')
+            type=str,
+            prompt=True,
+            help='The client email')
 @click.option('-p', '--position', 
-              type=str,
-              prompt=True,
-              help='The client position')
+            type=str,
+            prompt=True,
+            help='The client position')
 @click.pass_context
 def create(ctx, name, company, email, position):
     """Create a new client"""
@@ -56,16 +56,63 @@ def list(ctx):
 
 
 @clients.command()
+@click.argument('client_uid',
+                type=str)
 @click.pass_context
 def update(ctx, client_uid):
     """Updates a client"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
 
+    client_list = client_service.list_clients()
+
+    client = [client for client in client_list if client['uid'] == client_uid]
+
+    if client:
+        client = _update_client_flow(CLient(**client[0]))
+        client_service.update_client(client)
+
+        click.echo('Client updated')
+    else:
+        click.echo('client not found')
+
+def _update_client_flow(client):
+    click.echo('Leave empty if you dont want to modify the value')
+
+    client.name = click.prompt('New name', type=str, default=client.name)
+    client.company = click.prompt('New company', type=str, default=client.company)
+    client.email = click.prompt('New email', type=str, default=client.email)
+    client.position = click.prompt('New position', type=str, default=client.position)
+
+    return client
 
 @clients.command()
+@click.argument('client_uid',
+                type=str)
 @click.pass_context
 def delete(ctx, client_uid):
     """Deletes a client"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+    client_list = client_service.list_clients()
+    client = [client for client in client_list if client['uid'] == client_uid]
+
+    if client:
+        client_to_delete = _delete_client_flow(CLient(**client[0]))
+
+        if client_to_delete:
+            client_service.delete_client(client_to_delete)
+            click.echo('Client deleted successfully')
+        else:
+            click.echo('Aborted')
+
+def _delete_client_flow(client):
+    click.echo('Client with UID: {uid} was found.'.format(uid=client.uid))
+
+    delete_confirmation = click.prompt(
+        'Delete? [y]/N', type=str, default='Y')
+
+    if delete_confirmation.upper() == 'Y':
+        return client
+
+    return None
 
 all = clients
--- clients/services.py
+++ clients/services.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 from clients.models import CLient
 
@@ -16,3 +17,35 @@ class ClientService:
             reader = csv.DictReader(f, fieldnames=CLient.schema())
             
             return list(reader)
+        
+    def update_client(self, updated_client):
+        clients = self.list_clients()
+
+        updated_clients = []
+
+        for client in clients:
+            if client['uid'] == updated_client.uid:
+                updated_clients.append(updated_client.to_dict())
+            else:
+                updated_clients.append(client)
+        
+        self._save_to_disk(updated_clients)
+    
+    def _save_to_disk(self, clients):
+        tmp_table_name = self.table_name + '.tmp'
+        with open(tmp_table_name, mode='a') as f:
+            write = csv.DictWriter(f, fieldnames=CLient.schema())
+            write.writerows(clients)
+        
+        os.remove(self.table_name)
+        os.rename(tmp_table_name, self.table_name)
+
+    def delete_client(self, client_to_be_deleted):
+        clients = self.list_clients()
+
+        for client in clients:
+            if client['uid'] == client_to_be_deleted.uid:
+                clients.remove(client)
+                break
+
+        self._save_to_disk(clients)
