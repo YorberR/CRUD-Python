a49c4dd94d9e5d6fd2e193bc6b2b78e1f7c3a446
YorberR <yorberrojas15@gmail.com>
2024-11-07 15:53:27 -0400

    update and delete function

--- main.py
+++ main.py
@@ -1,33 +1,67 @@
-from setuptools import Command
 clients = "pablo,ricardo,"
+
 def create_cliente(client_name):
     global clients
+
     if client_name not in clients:
         clients += client_name
         _add_comma()
     else:
         print("Client already is in the clients list")
+
+
 def list_clints():
     global clients
+
     print(clients)
+
+
+def update_client(cliente_name, updated_client_name):
+    global clients
+    if cliente_name in clients:
+        clients = clients.replace(client_name + ",", updated_client_name + ",")
+    else:
+        print("Client is not in clients list")
+def delete_client(client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name + ",", "")
+    else:
+        print("Client is not in clients list")
 def _add_comma():
     global clients
+
     clients += ","
+
+
 def _print_welcom():
     print("WELCONE TO MY STORE")
     print("*" * 50)
     print("What world you like to do today?")
     print("[C]reate clint")
+    print("[U]pdate client")
     print("[D]elete client")
+
+def _get_client_name():
+    return input("What is the client name?")
+
 if __name__ == "__main__":
     _print_welcom()
+
     Command = input()
-    if Command == "c":
-        client_name = input("What is the client name?")
+    Command = Command.upper()
+
+    if Command == "C":
+        client_name = _get_client_name()
         create_cliente(client_name)
         list_clints()
-    elif Command == "d":
-        pass
+    elif Command == "D":
+        client_name = _get_client_name()
+        delete_client(client_name)
+    elif Command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input("What is the updated client name")
+        update_client(client_name, updated_client_name)
     else:
         print("Invalid command")
-    print(clients)
+
